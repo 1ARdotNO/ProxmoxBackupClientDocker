5cf8e3c858097662721630fc7b318f8451423e20
Einar Stenberg <einar@stenberg.im>
2020-08-05 14:26:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 
 #insert cronjob
-"$CRON /bin/bash /backupscript.sh" > /var/spool/cron/root
-top
+"$CRON /usr/bin/pwsh /backupscript.sh" > /var/spool/cron/root
+
 while /bin/true; do
   sleep 60
 done
