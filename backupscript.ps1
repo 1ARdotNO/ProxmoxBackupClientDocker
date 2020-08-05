6097ce34d658a3be939d73b1f4f7fa0e1466fa1d
Einar Stenberg <einar@stenberg.im>
2020-08-05 13:30:29 +0200

    Update backupscript.ps1

--- backupscript.ps1
+++ backupscript.ps1
@@ -10,8 +10,17 @@ if($ENV:PRESCRIPT){
 ##Run backupjob
 
 if($ENV:PBS_PASSWORD -and $ENV:PBS_REPOSITORY -and $ENV:ARCHIVENAME){
-  proxmox-backup-client backup $ENV:ARCHIVENAME.pxar:/root
+  #create args
+  $backupargs=backup $ENV:ARCHIVENAME.pxar:/$ENV:SOURCEDIR
+  #start the backup process
+  Start-Process -Args $backupargs -FilePath proxmox-backup-client
 }
 else {
   write-host "MISSING VARIABLES"
+}
+
+##Run post script
+if($ENV:POSTSCRIPT){
+  write-host "Running Post-script from $($ENV:POSTSCRIPT)"
+  . $ENV:POSTSCRIPT
 }
