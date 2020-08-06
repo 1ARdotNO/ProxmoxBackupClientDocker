4b167de2503ee418ef844ef72d06fede51e61ce7
Einar Stenberg <einar@stenberg.im>
2020-08-06 12:16:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 #create cronjob
 echo "$CRON . /root/env.sh; /usr/bin/pwsh -File /backupscript.sh" | crontab -
 #create env's for cron
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/env.sh 
+pwsh -command "Get-ChildItem env: | Export-CliXml /env-vars.clixml"
 #start cron service
 service cron start
 while /bin/true; do
