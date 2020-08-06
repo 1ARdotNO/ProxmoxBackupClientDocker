a83beafd7806eb5bbad3a6b0f925a4d10f855cac
Einar Stenberg <einar@stenberg.im>
2020-08-06 11:42:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 
 
-#insert cronjob
-echo "$CRON /usr/bin/pwsh -File /backupscript.sh" | crontab -
+#create cronjob
+echo "$CRON . /root/env.sh; /usr/bin/pwsh -File /backupscript.sh" | crontab -
+#create env's for cron
+printenv | sed 's/^\(.*\)$/export \1/g' > /root/env.sh 
+#start cron service
 service cron start
 while /bin/true; do
   sleep 60
