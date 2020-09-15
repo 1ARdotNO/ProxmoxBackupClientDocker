add54fb9f069b80fbda58fb355f961fb4ce737c8
Einar Stenberg <einar@stenberg.im>
2020-09-15 12:33:09 +0200

    Create googlecloud.ps1

---
+++ prescripts/googlecloud.ps1
@@ -0,0 +1,24 @@
+#install GSUTIL
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+      
+
+function GCP-Backup{
+param(
+$bucketname,
+$backupdir,
+$authfile,
+
+)
+
+#create folder
+$targetdir="$backupdir\$(get-date -Format yyyyMMdd-HHmm)"
+new-item $targetdir -ItemType Directory -Force
+
+
+#import credentials
+gcloud auth activate-service-account --key-file=$authfile
+
+#run backup
+. gsutil -m rsync -L $targetdir\log.txt -r $bucketname $targetdir 
+}
+GCP-Backup -bucketname gs://$ENV:GCP_BUCKETNAME/ -backupdir $ENV:GCP_BACKUPDIR -authfile $ENV:GCP_AUTHFILE
