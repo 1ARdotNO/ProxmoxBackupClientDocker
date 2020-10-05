f20db118c41c6d5f052ed01037ebf95f3c70f0c6
fintechestenberg <50585165+fintechestenberg@users.noreply.github.com>
2020-10-05 11:05:43 +0200

    Update reporting.ps1

--- reporting.ps1
+++ reporting.ps1
@@ -12,9 +12,24 @@ $errorlines=$transcript | Select-String -Pattern $Errorstrings
 
 if($env:elastic_host){
     
-    @{
+    $data=@{
       log=$transcript
       status=$status
       errorlines=$errorlines
+    } | convertto-json
+    dbinfo=@{
+        server = "$ENV:elastic_server"
+        user = "$ENV:elastic_username"
+        password = "$ENV:elastic_password"
+        protocol = "$ENV:elastic_protocol"
+        index = "$ENV:elastic_index"
     }
+    #if index does not exist create it
+    if(Get-Elasticindex @dbinfo ){}
+    else{
+        New-Elasticindex @dbinfo
+    sleep 10
+    }
+    add-elasticdata @dbinfo -body $data
+    
 }
