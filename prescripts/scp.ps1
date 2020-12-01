a7f06723d927aa89ced1ee0833de165442ddbd14
Einar Stenberg <einar@stenberg.im>
2020-12-01 17:27:25 +0100

    Update scp.ps1

--- prescripts/scp.ps1
+++ prescripts/scp.ps1
@@ -1,4 +1,4 @@
   
 ##Script to download file(s) from scp for backup
 mkdir $ENV:SCP_TARGET
-sshpass -p $ENV:SCP_PASSWORD scp -r $ENV:SCP_USER@$ENV:SCP_HOST:$ENV:SCP_SOURCE $ENV:SCP_TARGET
+sshpass -p "$($ENV:SCP_PASSWORD)" scp -o StrictHostKeyChecking=no -r $ENV:SCP_USER@$($ENV:SCP_HOST):$($ENV:SCP_SOURCE) $ENV:SCP_TARGET
