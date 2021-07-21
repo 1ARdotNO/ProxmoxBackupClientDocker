64a86be47496bccb0c730d508187e801c545aeda
fintechestenberg <estenberg@fintechinnovation.no>
2021-07-21 09:30:45 +0200

    add function for immediate run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 
-
-#create cronjob
-echo "$CRON /usr/bin/pwsh -File /backupscript.ps1" | crontab -
 #create env's for cron
 pwsh -command "Get-ChildItem env: | Export-CliXml /env-vars.clixml"
+
+#if CRON not set, trigger job immediatly then exit the container
+if [ -z ${CRON} ]
+then 
+#create cronjob
+echo "$CRON /usr/bin/pwsh -File /backupscript.ps1" | crontab -
 #start cron service
 service cron start
 while /bin/true; do
   sleep 60
 done
+else
+pwsh /backupscript.ps1
+fi
+
+
