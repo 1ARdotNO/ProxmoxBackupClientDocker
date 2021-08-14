59e53a9761f2c8bf9bc36a4def66b452a4943869
Einar Stenberg <einar@stenberg.im>
2021-08-14 23:10:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@
 pwsh -command "Get-ChildItem env: | Export-CliXml /env-vars.clixml"
 
 #if CRON not set, trigger job immediatly then exit the container
-if [ -z ${CRON} ]
+if [ -z ${CRON+x} ];
 then 
+pwsh /backupscript.ps1
+else
 #create cronjob
 echo "$CRON /usr/bin/pwsh -File /backupscript.ps1" | crontab -
 #start cron service
@@ -14,8 +16,4 @@ service cron start
 while /bin/true; do
   sleep 60
 done
-else
-pwsh /backupscript.ps1
 fi
-
-
