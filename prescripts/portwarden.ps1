17f21a685a2e3b9189217b270ed761d977183f3f
Einar Stenberg <einar@stenberg.cloud>
2022-06-07 00:17:18 +0200

    add password for portwarden export

--- prescripts/portwarden.ps1
+++ prescripts/portwarden.ps1
@@ -1,1 +1,3 @@
-portwarden --passphrase $ENV:PORTWARDEN_PASSPHRASE --filename $ENV:SOURCEDIR/$ENV:PORTWARDEN_VAULTNAME encrypt
+"Starting Portwarden..."
+echo $ENV:BW_PASSWORD | portwarden --passphrase $ENV:PORTWARDEN_PASSPHRASE --filename $ENV:SOURCEDIR/$ENV:PORTWARDEN_VAULTNAME encrypt
+"Portwarden complete!"
