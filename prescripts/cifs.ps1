de940558fa60a97596019d0bc2df9fb7f9c7db42
Einar Stenberg <einar@stenberg.cloud>
2023-01-16 11:01:33 +0000

    add debug

--- prescripts/cifs.ps1
+++ prescripts/cifs.ps1
@@ -7,5 +7,6 @@
 if(test-path /mnt/cifs){}
 else {mkdir /mnt/cifs}
 
+"mounting cifs..."
 mount -t cifs $ENV:CIFS_UNC /mnt/cifs -o username=$ENV:CIFS_USER,password=$ENV:CIFS_PASSWORD,domain=$ENV:CIFS_DOMAIN,ro
 
